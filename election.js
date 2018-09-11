9d30cadd3603342d73e3bc660fba43aed2f1f88a
David Ma <davidma415@gmail.com>
2018-09-11 19:30:28 -0400

    Finished part 1 of assignment

--- election.js
+++ election.js
@@ -1,5 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
+  var candidateButton = document.getElementById('button1')
+  candidateButton.addEventListener('click', returnCandidates);
 
-  // Imagination!
-
+  function returnCandidates() {
+    $.ajax({
+      url: 'https://bb-election-api.herokuapp.com/',
+      method: 'GET',
+      data: {},
+      dataType: 'json'
+    }).done(function(responseData) {
+      var listCandidates = document.getElementById('candidates-list')
+      for (var i = 0; i < responseData.candidates.length; i++) {
+        listItem = document.createElement('li');
+        listItem.innerHTML = 'Name: ' + responseData.candidates[i].name + ' Votes: ' + responseData.candidates[i].votes;
+        listCandidates.append(listItem);
+      }
+    });
+  }
 });
